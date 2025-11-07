6923e7fd77861d181ad6804a8658767dc16f08d7
zyter <ayoub.meftah2006@gmail.com>
2025-11-07 01:26:53 +0100

    travail terminer

---
+++ exo1/script.js
@@ -0,0 +1,27 @@
+"use strict";//cours :p37 :mode strict pour eviter les variable non declarer
+//cours :p37 : declaration de variable
+let nom = "Doe";
+let prenom = "John";
+let age = 30;
+let note = 15;
+let pi = 3.14;
+//cours : p45 : declaration de function
+function f1() {
+    console.log(nom);//cours : p33 : utilisation de console.log
+}
+
+function f2() {
+    console.log(prenom);
+}
+function f3() {
+    console.log(nom + " " + prenom);//cours : p41 : concatenation de chaine de caractere
+}
+function f4() {
+    console.log(age);
+}
+function f5() {
+    console.log(note);
+}
+function f6() {
+    console.log(pi);
+}
